5e5680066a7a980d1790e53bb811c30796ab3b75
Sheng-Liang Slogar <sheng@diglactic.com>
2019-10-18 09:14:29 -0600

    Refactor

--- static/script.js
+++ static/script.js
@@ -1,8 +1,4 @@
-{
-  window.dataLayer = window.dataLayer || [];
-
-  function gtag() {dataLayer.push(arguments);}
-
-  gtag('js', new Date());
-  gtag('config', 'UA-38963765-4');
-}
+window.dataLayer = window.dataLayer || [];
+function gtag() {dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-38963765-4');
